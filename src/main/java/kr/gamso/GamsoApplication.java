ebeb67ed88d1ceddc0c021f604b16854bee8d9b1
이형승 <dlgudtmd1022@gamil.com>
2023-07-03 19:02:31 +0900

    first

---
+++ src/main/java/kr/gamso/GamsoApplication.java
@@ -0,0 +1,13 @@
+package kr.gamso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamsoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GamsoApplication.class, args);
+    }
+
+}
