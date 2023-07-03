522e4f54a4361b76a150d239f7d13e96e649f4f2
이형승 <dlgudtmd1022@gamil.com>
2023-07-03 20:11:17 +0900

    db 변경

--- sql/gamso_db.sql
+++ sql/gamso_db.sql
@@ -1,28 +1,32 @@
 CREATE TABLE users(
-	id INT PRIMARY KEY AUTO_INCREMENT,
-    email TEXT NOT NULL,
-    pw VARCHAR(15) NOT NULL,
-    usage_count INT DEFAULT 0,
-    signup_date DATETIME DEFAULT now()
+                      member_number INT PRIMARY KEY AUTO_INCREMENT,
+                      id VARCHAR(20) NOT NULL,
+                      email TEXT NOT NULL,
+                      pw VARCHAR(15) NOT NULL,
+                      signup_date DATETIME DEFAULT now()
 );
 
 CREATE TABLE url(
-	id INT NOT NULL, 
-	original_url TEXT NOT NULL,
-    shorten_url TEXT NOT NULL,
-    shorten_date DATETIME DEFAULT now(),
-    CONSTRAINT fk_id FOREIGN KEY (id) REFERENCES users(id)
+                    member_number INT NOT NULL,
+                    original_url TEXT NOT NULL,
+                    shorten_url TEXT NOT NULL,
+                    shorten_date DATETIME DEFAULT now(),
+                    CONSTRAINT fk_member_number FOREIGN KEY (member_number) REFERENCES users(member_number)
 );
 
-INSERT INTO users (email, pw) VALUES 
-			("asd@naver.com", "1234"),
-            ("zxc@google.com","4567"),
-            ("qwe@gamso.kr", "99");
+INSERT INTO users (id, email, pw) VALUES
+                                      ("asd", "hjk@naver.com", "1234"),
+                                      ("zxccxvxcv", "hyt@google.com", "4567"),
+                                      ("qwe", "bnm@naver.com", "99");
+
+INSERT INTO url (member_number, original_url, shorten_url) VALUES
+                                                               (1,"www.naver.com","gamso.kr/11sxw2"),
+                                                               (2,"www.google.com","gamso.kr/32sad"),
+                                                               (2,"www.naver.com","gamso.kr/32sad"),
+                                                               (2,"www.daum.com", "gamso.kr/weq22");
 
 SELECT * FROM users;
-INSERT INTO url (id,original_url, shorten_url) VALUES 
-				(1,"www.naver.com","gamso.kr/11sxw2"),
-                (2,"www.google.com","gamso.kr/32sad"),
-                (2,"www.naver.com","gamso.kr/32sad"),
-                (3,"www.daum.com", "gamso.kr/weq22");
+SELECT * FROM url;
 
+drop table url;
+drop table users;
