3a7e32d93f8b8278cd91c9f46610d18c4385b314
smjteresa95 <smjteresa95@gmail.com>
2023-07-17 17:57:10 +0900

    login 뷰, 기능을 위한 컨트롤러 구현

--- src/main/java/kr/gamso/controller/UsersController.java
+++ src/main/java/kr/gamso/controller/UsersController.java
@@ -5,6 +5,7 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,4 +30,10 @@ public class UsersController {
     public String goSignUp(){
         return "signUp";
     }
+
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+
 }
--- src/main/java/kr/gamso/service/UsersServiceImpl.java
+++ src/main/java/kr/gamso/service/UsersServiceImpl.java
@@ -1,33 +1,65 @@
 package kr.gamso.service;
 
+import com.mysql.cj.x.protobuf.MysqlxCrud;
+import kr.gamso.dto.userDTO.AddUserRequestDTO;
 import kr.gamso.dto.userDTO.FindUserDTO;
 import kr.gamso.dto.userDTO.SaveUserDTO;
+import kr.gamso.entity.Users;
 import kr.gamso.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.apache.catalina.User;
+import org.springframework.security.core.parameters.P;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UsersServiceImpl implements UsersService{
-    UserRepository userRepository;
-    @Autowired
-    public UsersServiceImpl(UserRepository userRepository){
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public UsersServiceImpl(UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder)
+    {
         this.userRepository = userRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
     }
 
     @Override
-    public List<FindUserDTO> findAllUsers() {
-        return userRepository.findAllUsers();
+    public List<FindUserDTO> findAll() {
+        List<Users> usersList = userRepository.findAll();
+        List<FindUserDTO> userDTOList = new ArrayList<>();
+        for(Users user : usersList) {
+            //entity -> dto
+            FindUserDTO userDTO = FindUserDTO.builder()
+                    .id(user.getId())
+                    .email(user.getEmail())
+                    .pw(user.getPw())
+                    .build();
+            userDTOList.add(userDTO);
+        }
+        return userDTOList;
     }
 
     @Override
-    public void insertUser(SaveUserDTO saveUserDTO) {
-        userRepository.saveUser(saveUserDTO);
+    public void insert(SaveUserDTO saveUserDTO) {
+        Users user = saveUserDTO.toEntity();
+        userRepository.save(user);
     }
 
     @Override
-    public int selectUserId(String id) {
-        return userRepository.selectUserId(id);
+    public boolean isIdDuplicate(String id) {
+        Optional<Users> existingUser = userRepository.findById(id);
+        return existingUser.isPresent();
     }
+
+    @Override
+    public Long save(AddUserRequestDTO dto) {
+        return userRepository.save(Users.builder()
+                .email(dto.getEmail())
+                .pw(bCryptPasswordEncoder.encode(dto.getPw()))
+                .build());
+    }
+
 }
